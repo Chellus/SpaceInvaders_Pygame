1b30f6ca6ec0b28581d3a2a283325a6fac3f9ecc
Elias Riveros <elias4845@gmail.com>
2020-10-31 15:38:20 -0300

    Added an enemy and a background

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #initialize pygame
 pygame.init()
@@ -7,10 +8,14 @@ window_w = 800 #width
 window_h = 600 #height
 
 spaceship_w = 64
+enemy_w = 64
 
 #create the window
 window = pygame.display.set_mode((window_w, window_h))
 clock = pygame.time.Clock()
+
+#Background
+background = pygame.image.load('resources/background.png')
 
 #Title and Icon
 pygame.display.set_caption("Space Invaders - pygame")
@@ -20,16 +25,29 @@ pygame.display.set_icon(icon)
 #Player
 playerImg = pygame.image.load('resources/player.png')
 playerX = 370
-playerY = 480
+playerY = 530
 playerX_change = 0
+
+#Enemy
+enemyImg = pygame.image.load('resources/enemy.png')
+enemyX = random.randint(5, 730)
+enemyY = 50
+enemyX_change = 4.5
+enemyY_change = 1.5 #the enemy goes down
 
 def player(x, y):
     window.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    window.blit(enemyImg, (x, y))
 
 running = True
 
 if __name__ == "__main__":
     while running:
+
+        window.blit(background, (0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -44,15 +62,26 @@ if __name__ == "__main__":
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     playerX_change = 0
 
-        #controlling boundaries of the window
+        #controlling boundaries of the window for the player
         if playerX + playerX_change > window_w - spaceship_w or playerX + playerX_change < 0:
             playerX_change = 0
 
+        #controlling boundaries of the window for the enemy on the X axis
+        if enemyX + enemyX_change > window_w - enemy_w:
+            enemyX_change = -2.5
+            enemyY += enemy_w #the width of the enemy is the same as the enemy height
+        elif enemyX + enemyX_change < 0:
+            enemyX_change = 2.5
+            enemyY += enemy_w
+
+        if enemyY + enemyY_change >= window_h - enemy_w:
+            running = False
+
         playerX += playerX_change
-        
-        window.fill((0, 100, 0)) #RGB values to fill screen
+        enemyX += enemyX_change
 
         player(playerX, playerY) #show the player
+        enemy(enemyX, enemyY) #show enemy
 
         pygame.display.update()
         clock.tick(60)
